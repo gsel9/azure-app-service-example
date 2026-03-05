31cd5d9bce0db25850b71522dca0425545b5510e
Elvatun <severin.elvatun@capgemini.com>
2026-03-05 16:14:04 +0100

    simpler example

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+import uvicorn
+
+app = FastAPI()
+
+
+@app.post("/ask")
+async def ask_api(request: Request):
+    response = {
+        "headers": request.headers,
+        "query_params ": request.query_params
+    }
+    return response
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8000)
