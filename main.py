ad00f3b0c83842ea116f54d7c8bed384b38361b3
Elvatun <severin.elvatun@capgemini.com>
2026-03-05 18:09:22 +0100

    simples possible main

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-from fastapi import FastAPI, Request
-import uvicorn
+from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/ask")
-async def ask_api(request: Request):
-    response = {
-        "headers": request.headers,
-        "query_params ": request.query_params
-    }
-    return {"response": "Hello to you!"}
-
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000)
+@app.get("/hello")
+def hello():
+    return {"msg": "Hello from Azure!"}
