a6a29a79773b2bdf37aa43c88ec214e522fb60e0
Elvatun <severin.elvatun@capgemini.com>
2026-03-06 13:47:11 +0100

    updates to startup script

--- startup.sh
+++ startup.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+echo "Starting application..."
+
+# install dependencies if needed
+pip install -r requirements.txt
+
 gunicorn -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT
