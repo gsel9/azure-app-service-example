8b5baf02b1f114d12e51fab2b5ea250db8222d15
Elvatun <severin.elvatun@capgemini.com>
2026-03-06 14:03:07 +0100

    removes startup script

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Starting application..."
-
-# install dependencies if needed
-pip install -r requirements.txt
-
-gunicorn -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT
